4ef4a3ccdddce968ce19c51bd17972b985d37043
deezy13 <deezy13@gmail.com>
2019-10-11 16:32:27 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -22,7 +22,7 @@ def speak_to_grandma(phrase)
     elsif phrase.upcase
      return "NO, NOT SINCE 1938!"
       
-    else phrase == "I LOVE YOU GRANDMA!"
+    else phrase == 'I LOVE YOU GRANDMA!'
       return "I LOVE YOU TOO PUMPKIN!"
 end 
 end 
